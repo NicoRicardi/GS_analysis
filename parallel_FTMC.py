baef09c0e9f935a070852a4c413889882a118358
NicoRicardi <niccolo.ricardi@unige.ch>
2021-08-10 17:34:22 +0200

    print stoud/sterr

--- parallel_FTMC.py
+++ parallel_FTMC.py
@@ -9,7 +9,7 @@ Created on Tue Aug 10 16:49:29 2021
 import subprocess as sp
 import sys
 
-systems = ["7HQ_2MeOH", "]7HQ_formate", "Uracil_5H2O", "XVI_2HCOOH"]
+systems = ["7HQ_2MeOH", "7HQ_formate", "Uracil_5H2O", "XVI_2HCOOH"]
 expansion = sys.argv[1]
 
 for system in systems:
@@ -17,4 +17,5 @@ for system in systems:
     script = "FTMC.py"
     command = "sbatch {slrm} python3 {script} {system} {expansion}".format(slrm=slrm, script=script, system=system, expansion=expansion) 
     print(command)
-    sp.run(command, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+    p = sp.run(command, stdout=sp.PIPE, stderr=sp.STDOUT, shell=True)
+    print(p.stdout)
