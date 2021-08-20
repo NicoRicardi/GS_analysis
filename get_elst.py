e3eebbe582dcbd9025fbf69e4c344b9a3e3376f4
NicoRicardi <niccolo.ricardi@unige.ch>
2021-08-20 18:14:16 +0200

    correct reqs functions

--- get_elst.py
+++ get_elst.py
@@ -39,15 +39,18 @@ import itertools as ittl
 
 cqlist = ["elst_change_ref", "elst_change_FDET"]
 reqs = {}
-reqs_sum_iso = ["FT-ME/cy0,J_sum_iso", "FT-ME/cy0,AnucB_sum_iso","FT-ME/cy0,BnucA_sum_iso"]
+reqs_sum_iso_pfunc = ["FT-ME/cy0,J_sum_iso", "FT-ME/cy0,AnucB_sum_iso","FT-ME/cy0,BnucA_sum_iso"]
+reqs_sum_iso_ccp = ["A_MP2,tot_coulomb", "A_MP2,nuc_attr", "A_MP2,nuc_repu",
+                    "B_MP2,tot_coulomb", "B_MP2,nuc_attr", "B_MP2,nuc_repu",
+                    "FT-ME/cy0,V_AB"]
 reqs_elst_AB = ["AB_MP2,tot_coulomb", "AB_MP2,nuc_attr", "AB_MP2,nuc_repu"]
-reqs_elst_FDET = ["A_MP2,tot_coulomb", "A_MP2_,nuc_attr", "A_MP2,nuc_repu",
-    "A_MP2,J_int", "A_MP2_,V_AB", "A_MP2,AnucB", "A_MP2,BnucA",
-    "B_MP2,tot_coulomb", "B_MP2_,nuc_attr", "B_MP2,nuc_repu"]
-reqs["elst_change_ref"] = reqs_sum_iso + ["FT-ME/cy0,V_AB"]  + reqs_elst_AB
-reqs["elst_change_FDET"] = reqs_sum_iso + ["FT-ME/cy0,V_AB"] + reqs_elst_FDET
-parser = {i: "ccp" for i in reqs_elst_AB + reqs_elst_FDET + ["FT-ME/cy0,V_AB"]}
-parser.update(**{i: "elst" for i in reqs_sum_iso})
+reqs_elst_FDET = ["MP2_A,tot_coulomb", "MP2_A,nuc_attr", "MP2_A,nuc_repu",
+    "MP2_A,J_int", "MP2_A,V_AB", "MP2_A,AnucB", "MP2_A,BnucA",
+    "MP2_A,tot_coulomb", "MP2_A,nuc_attr", "MP2_A,nuc_repu"]
+reqs["elst_change_ref"] = reqs_sum_iso_pfunc + reqs_sum_iso_ccp + reqs_elst_AB
+reqs["elst_change_FDET"] = reqs_sum_iso_pfunc + reqs_sum_iso_ccp + reqs_elst_FDET
+parser = {i: "ccp" for i in reqs_elst_AB + reqs_elst_FDET + reqs_sum_iso_ccp}
+parser.update(**{i: "elst" for i in reqs_sum_iso_pfunc})
 parserfuncs = {"ccp": None, "elst": pfunc}
       
 joblist = [("WORK","database","folder"), ("WORK","database","folder2")]
@@ -57,7 +60,7 @@ systems = ["Uracil_5H2O"]
 #calcs = ["FT-ME", "MC-nopp", "MC-pp_Mulliken", "MC_pp_ChelPG"]
 calcs = ["FT-ME"]
 joblist = [i for i in ittl.product([cwd], systems, calcs)]
-df = ccd.myfunc(joblist, levelnames=["base","system","calc"], qlist=cqlist,
+df = ccd.collect_data(joblist, levelnames=["base","system","calc"], qlist=cqlist,
                  reqs=reqs, ext="*.out", ignore="slurm*", parser=parser, parserfuncs=parserfuncs,
                  parser_file="CCParser.json",parser_args=None, 
                  parser_kwargs=None, check_input=True, funcdict = "ccp",
